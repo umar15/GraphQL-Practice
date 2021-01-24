c6160d198cc76cda8980ad195f62c3f3f4807ed9
Umar Khalid <umar.qur105@gmail.com>
2021-01-24 22:18:05 +0500

    Mutation done

--- graphql-client/src/components/Books.js
+++ graphql-client/src/components/Books.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
 const Get_Books = gql`
 	{
 		books {
+			id
+			title
+			author
+		}
+	}
+`;
+
+const Add_Book = gql`
+	mutation AddBook($id: Int!, $title: String!, $author: String!) {
+		addBook(input: { id: $id, title: $title, author: $author }) {
+			id
 			title
 			author
 		}
@@ -12,17 +23,36 @@ const Get_Books = gql`
 
 function Books() {
 	const { loading, error, data } = useQuery(Get_Books);
+	const [AddBooks] = useMutation(Add_Book);
 
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error :(</p>;
 
-	return data.books.map(({ title, author }) => (
-		<div key={title}>
-			<p>
-				{title}: {author}
-			</p>
+	return (
+		<div>
+			{data.books.map(({ id, title, author }) => (
+				<div key={id}>
+					<p>
+						{title}: {author}
+					</p>
+				</div>
+			))}
+			<button
+				onClick={() =>
+					AddBooks({
+						variables: {
+							id: 4,
+							title: "abc",
+							author: "umar",
+						},
+						refetchQueries: [{ query: Get_Books }],
+					})
+				}
+			>
+				Add Book
+			</button>
 		</div>
-	));
+	);
 }
 
 export default Books;
--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,7 +1,21 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const books = [
+	{
+		id: 1,
+		title: "The Awakening",
+		author: "Kate Chopin",
+	},
+	{
+		id: 2,
+		title: "City of Glass",
+		author: "Paul Auster",
+	},
+];
+
 const typeDefs = gql`
 	type Book {
+		id: Int
 		title: String
 		author: String
 	}
@@ -9,22 +23,35 @@ const typeDefs = gql`
 	type Query {
 		books: [Book]
 	}
-`;
 
-const books = [
-	{
-		title: "The Awakening",
-		author: "Kate Chopin",
-	},
-	{
-		title: "City of Glass",
-		author: "Paul Auster",
-	},
-];
+	input BookInput {
+		id: Int
+		title: String
+		author: String
+	}
+
+	type Mutation {
+		addBook(input: BookInput): Book
+	}
+`;
 
 const resolvers = {
 	Query: {
 		books: () => books,
+	},
+	Mutation: {
+		addBook: ({ input }) => {
+			books.push({
+				id: input.id,
+				title: input.title,
+				author: input.author,
+			});
+			return {
+				id: input.id,
+				title: input.title,
+				author: input.author,
+			};
+		},
 	},
 };
 
@@ -33,3 +60,17 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
 	console.log(`🚀  Server ready at ${url}`);
 });
+
+// {
+//   books{
+//     title
+//     author
+//   }
+// }
+// mutation {
+//   addBook(input: { id: 3, title: "new book", author: "xyz" }) {
+//     id
+//     title
+//     author
+//   }
+// }
